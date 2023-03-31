7043259c1affb50d805c3f50e22441bc58b82305
David Miller <15317284+pcktbot@users.noreply.github.com>
2023-03-31 10:36:19 -0500

    update to include spam generator

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import { PubSub } from '@google-cloud/pubsub';
 import { config } from 'dotenv';
+import asyncPool from 'tiny-async-pool';
 
 config();
 const app = express();
@@ -8,11 +9,20 @@ const app = express();
 const {
   PUBSUB_TOPIC,
   PUBSUB_SUBSCRIPTION,
-  PUBSUB_PROJECT_ID
+  PUBSUB_PROJECT_ID,
+  POOL_CONCURRENCY
 } = process.env;
 
 
 const pubsub = new PubSub({ projectId: PUBSUB_PROJECT_ID });
+
+const publish = async (data) => {
+  const dataBuffer = Buffer.from(JSON.stringify(data));
+  console.info(`📫 Publishing message to ${PUBSUB_TOPIC}...`, dataBuffer);
+  return await pubsub.topic(PUBSUB_TOPIC).publishMessage({ data: dataBuffer });
+  // FOR DEBUGGING, UNCOMMENT THE FOLLOWING LINE AND COMMENT THE ABOVE LINE
+  // return new Promise((resolve, reject) => setTimeout(() => resolve('📫 Message published'), 1000));
+};
 
 const subscribe = (subscriptionId) => {
   const subscription = pubsub.subscription(subscriptionId);
@@ -29,8 +39,26 @@ const handleMessage = (message) => {
   message.ack();
 };
 
+const bundleMessages = async (messages) => {
+  const results = [];
+  for await (const result of asyncPool(POOL_CONCURRENCY, messages, publish)) {
+    results.push(result);
+  }
+  return results;
+}
+
 const subscription = subscribe(PUBSUB_SUBSCRIPTION);
 
 subscription.on('message', message => handleMessage(message));
+
+app.use(express.json());
+
+app.post('/publish', async (req, res) => {
+  const { message, count } = req.body;
+  const messages = Array(count).fill(message);
+  console.info({ messages });
+  const messageIds = await bundleMessages(messages);
+  res.send(messageIds);
+});
 
 app.listen(3003, () => console.log('📭 Server is running on port 3003'));
